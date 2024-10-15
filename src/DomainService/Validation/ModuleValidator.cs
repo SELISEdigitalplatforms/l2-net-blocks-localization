e95ef08204b59bda71aa56f8c077576cb79c018d
Koushik Ghosh <koushik.ghosh@selise.ch>
2024-10-15 14:52:58 +0600

    update validation

--- src/DomainService/Validation/ModuleValidator.cs
+++ src/DomainService/Validation/ModuleValidator.cs
@@ -1,14 +1,27 @@
-﻿using FluentValidation;
+﻿using DomainService.Repositories;
+using FluentValidation;
 
 namespace DomainService.Services
 {
     public class ModuleValidator : AbstractValidator<Module>
     {
-        public ModuleValidator()
+        private readonly  IModuleRepository _moduleRepository;
+
+        public ModuleValidator(IModuleRepository moduleRepository)
         {
+            _moduleRepository = moduleRepository;
+
             RuleFor(module => module.ModuleName)
                 .NotEmpty().WithMessage("Module name is required.")
+                .MustAsync(async (name, cancellationToken) => await IsNameUniqueAsync(name))
+                .WithMessage("The name must be unique.")
                 .Length(3, 100).WithMessage("Module name must be between 3 and 100 characters long.");
+        }
+
+        private async Task<bool> IsNameUniqueAsync(string name)
+        {
+            var configuration = await _moduleRepository.GetByNameAsync(name);
+            return configuration == null;
         }
     }
 }
