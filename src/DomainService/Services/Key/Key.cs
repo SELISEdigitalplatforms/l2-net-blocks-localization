56a53d60eeaca68fe4a970be2b3754fb2b9cadcd
rifathsahriar <114345102+rifathsahriar@users.noreply.github.com>
2025-01-28 19:13:26 +0600

    removed value from Key.cs

--- src/DomainService/Services/Key/Key.cs
+++ src/DomainService/Services/Key/Key.cs
@@ -10,7 +10,6 @@ namespace DomainService.Services
         public string ItemId { get; set; }
         public string KeyName { get; set; }
         public string ModuleId { get; set; }
-        public string Value { get; set; }
         public Resource[] Resources { get; set; }
         public List<string> Routes { get; set; }
         public bool IsPartiallyTranslated { get; set; }
