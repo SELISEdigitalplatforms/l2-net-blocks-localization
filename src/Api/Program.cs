ada40c3356c05dc4139d3d09ec97b53cea3b4512
Tahmid Hossain <tahmid.hossain@selisegroup.com>
2025-05-21 16:17:55 +0600

    code for on prem

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -3,17 +3,47 @@ using Blocks.Genesis;
 using DomainService.Utilities;
 
 const string _serviceName = "blocks-localization-api";
-var blocksSecret = await ApplicationConfigurations.ConfigureLogAndSecretsAsync(_serviceName, VaultType.Azure);
+
+var blocksSecrets = await ApplicationConfigurations.ConfigureLogAndSecretsAsync(_serviceName, VaultType.OnPrem);
+blocksSecrets.CacheConnectionString = "10.10.6.15:6379,abortConnect=false,connectTimeout=50000,syncTimeout=50000";
+blocksSecrets.MessageConnectionString = "amqp://guest:guest@10.10.6.15:5672/";
+blocksSecrets.LogConnectionString = "mongodb://10.10.6.15:27017/";
+blocksSecrets.MetricConnectionString = "mongodb://10.10.6.15:27017/";
+blocksSecrets.TraceConnectionString = "mongodb://10.10.6.15:27017/";
+blocksSecrets.LogDatabaseName = "Logs";
+blocksSecrets.MetricDatabaseName = "Metrics";
+blocksSecrets.TraceDatabaseName = "Traces";
+blocksSecrets.DatabaseConnectionString = "mongodb://10.10.6.15:27017/";
+blocksSecrets.RootDatabaseName = "Blocks-Root";
+blocksSecrets.EnableHsts = true;
+
+var messageConfiguration = new MessageConfiguration
+{
+    Connection = blocksSecrets.MessageConnectionString,
+    RabbitMqConfiguration = new()
+    {
+        ConsumerSubscriptions = new()
+        {
+            ConsumerSubscription.BindToQueue("blocks_localization_listener", 2),
+        }
+    },
+    ServiceName = blocksSecrets.ServiceName,
+};
 var builder = WebApplication.CreateBuilder(args);
 
 ApplicationConfigurations.ConfigureApiEnv(builder, args);
-var services = builder.Services;
 
-services.AddHealthChecks();
-builder.Services.RegisterApplicationServices();
-ApplicationConfigurations.ConfigureServices(services, Constants.GetMessageConfiguration());
+var services = builder.Services;
+messageConfiguration.Connection = "mongodb://10.10.6.15:27017/";
+
+ApplicationConfigurations.ConfigureServices(services, messageConfiguration);
 ApplicationConfigurations.ConfigureApi(services);
 
+services.RegisterApplicationServices();
+services.AddHealthChecks();
+
 var app = builder.Build();
+
 ApplicationConfigurations.ConfigureMiddleware(app);
+
 await app.RunAsync();
