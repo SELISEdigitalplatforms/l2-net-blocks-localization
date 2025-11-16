f7d9a25562a14b821fe16bcf5740ccbe9ebf2fa6
Rifath Sahriar <114345102+rifathsahriar@users.noreply.github.com>
2025-11-16 17:16:31 +0600

    webhook added for UilmImportEvent

--- src/Worker/Consumers/UilmImportEventConsumer.cs
+++ src/Worker/Consumers/UilmImportEventConsumer.cs
@@ -1,5 +1,6 @@
 using Blocks.Genesis;
 using DomainService.Services;
+using DomainService.Services.HelperService;
 using DomainService.Shared.Events;
 
 namespace Worker.Consumers
@@ -8,14 +9,21 @@ namespace Worker.Consumers
     public class UilmImportEventConsumer : IConsumer<UilmImportEvent>
     {
         private readonly IKeyManagementService _keyManagementService;
+        private readonly IWebHookService _webHookService;
 
-        public UilmImportEventConsumer(IKeyManagementService keyManagementService)
+        public UilmImportEventConsumer(IKeyManagementService keyManagementService, IWebHookService webHookService)
         {
             _keyManagementService = keyManagementService;
+            _webHookService = webHookService;
         }
         public async Task Consume(UilmImportEvent @event)
         {
             var isSuccess = await _keyManagementService.ImportUilmFile(@event);
+            if (isSuccess)
+            {
+                _webHookService.CallWebhook(new { UilmImportEvent = @event, IsSuccess = isSuccess});
+            }
+
         }
     }
 }
