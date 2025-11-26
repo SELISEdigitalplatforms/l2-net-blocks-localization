0e8685b48c5c1f97fca6182213e0a3a69dc016c1
theshameem75 <shameem.alam@selisegroup.com>
2025-11-26 16:38:21 +0600

    fix: removed nested key value on generate uilm file

--- src/DomainService/Services/Key/KeyManagementService.cs
+++ src/DomainService/Services/Key/KeyManagementService.cs
@@ -625,7 +625,7 @@ namespace DomainService.Services
 
                 string resourceValue = resource == null ? "[ KEY MISSING ]" : resource.Value;
 
-                AssignToDictionary(dictionary: dictionary, keyPath: reosurceKey.KeyName, value: resourceValue);
+                dictionary[reosurceKey.KeyName] = resourceValue;
             });
         }
         private void AssignResourceKeysToDictionaryForKeyMode(
